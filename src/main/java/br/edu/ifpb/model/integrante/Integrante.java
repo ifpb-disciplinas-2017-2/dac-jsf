9ef1ca9e5208083ed6afb1e2ec4b1af7d427c001
romulo-soares <bromulosoares@gmail.com>
2017-11-28 08:52:51 -0300

    CRUD integrante

---
+++ src/main/java/br/edu/ifpb/model/integrante/Integrante.java
@@ -0,0 +1,79 @@
+
+package br.edu.ifpb.model.integrante;
+
+import java.util.Objects;
+
+public class Integrante {
+    
+    private int id;
+    private String nome;
+    private CPF cpf;
+    
+    public Integrante(){
+        
+    }
+
+    public Integrante(int id, String nome, CPF cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(CPF cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + this.id;
+        hash = 37 * hash + Objects.hashCode(this.nome);
+        hash = 37 * hash + Objects.hashCode(this.cpf);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Integrante other = (Integrante) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.cpf, other.cpf)) {
+            return false;
+        }
+        return true;
+    }
+       
+}

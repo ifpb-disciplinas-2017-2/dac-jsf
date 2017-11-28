30d19192d88049e762c3220b2de31e5dc7409723
joseferreira01 <joseifpb2015@gmail.com>
2017-11-28 09:39:58 -0200

    tb Album jdbc

--- src/main/java/br/edu/ifpb/infra/persistence/jdbc/AlbunsEmJDBC.java
+++ src/main/java/br/edu/ifpb/infra/persistence/jdbc/AlbunsEmJDBC.java
@@ -3,9 +3,18 @@ package br.edu.ifpb.infra;
 import br.edu.ifpb.domain.model.album.Album;
 import br.edu.ifpb.domain.model.album.Albuns;
 import br.edu.ifpb.domain.model.banda.Integrante;
+import br.edu.ifpb.infra.persistence.jdbc.Conexao;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Ricardo Job
@@ -13,26 +22,59 @@ import java.util.concurrent.CopyOnWriteArrayList;
  * @since 20/11/2017, 10:27:39
  */
 public class AlbunsEmJDBC implements Albuns {
+     
+    private Conexao conexao;
 
-    private static final List<Album> albuns = new CopyOnWriteArrayList<>();
+    public AlbunsEmJDBC() {
+        conexao = new Conexao();
+    }
+    
+
+ //   private static final List<Album> albuns = new CopyOnWriteArrayList<>();
 
     @Override
     public boolean salvar(Album album) {
-        return albuns.add(album);
+        boolean re = false;
+        String sql = "INSERT INTO Album (descricao,dataDeLancamento) VALUES(?,?)";
+            PreparedStatement statement = null;
+        try {
+            statement = conexao.init().prepareStatement(sql);
+        
+            statement.setString(1, album.getDescricao());
+             statement.setDate(2, Date.valueOf(album.getDataDeLancamento()));
+             re = statement.execute();
+    }   catch (SQLException ex) {
+            Logger.getLogger(AlbunsEmJDBC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return re;
     }
 
     @Override
     public List<Album> listarTodos() {
-        return Collections.unmodifiableList(albuns);
+        try {
+            String consulta = "SELECT * FROM Album";
+
+            PreparedStatement statement = conexao.init().prepareStatement(consulta);
+            return criarPessoa(statement);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AlbunsEmJDBC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Collections.emptyList();
+        
     }
 
     @Override
     public void excluir(Album albumParaExcluir) {
-        this.albuns.remove(albumParaExcluir);
+        
     }
 
     @Override
     public Album localizarPor(String descricao) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private List<Album> criarPessoa(PreparedStatement statement) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
---
+++ src/main/java/br/edu/ifpb/infra/persistence/jdbc/tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Banda(
+id SERIAL PRIMARY KEY,
+nomeFantasia VARCHAR(100)
+);
+CREATE TABLE INTEGRANTE(
+id serial PRIMARY KEY,
+nome varchar(50),
+CPF VARCHAR(15),
+id_banda int,
+FOREIGN KEY (id_banda) REFERENCES danda(id) ON DELETE RESTRICT
+
+);
+CREATE TABLE Musica(
+ id serial PRIMARY KEY,
+nome varchar(50)
+);
+CREATE TABLE Album(
+id serial PRIMARY KEY,
+descricao varchar(50),
+ dataDeLancamento DATE
+
+)
